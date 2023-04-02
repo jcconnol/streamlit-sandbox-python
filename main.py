9fe21b1a694136ac649590c51ac0f5d20bd59957
John Connolly <jcconnol4@gmail.com>
2023-04-01 21:52:09 -0500

    add random tests

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
